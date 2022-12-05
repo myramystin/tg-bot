9af2c7b902ab9e131f963738011a4954e2e75ff4
dash <60468091+myramystin@users.noreply.github.com>
2022-12-05 22:26:12 +0300

    remove api key

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telebot import types
 import requests
 from datetime import datetime
 
-TOKEN = '5758333321:AAGUFuqY864Mtfy9Yo9ZCF1ERZVOeNV2gTQ'
+TOKEN = ''
 bot = telebot.TeleBot(TOKEN)
 
 question_ids = dict()
